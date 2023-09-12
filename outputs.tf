8e23ceb38f9ecbc70f3515bb6d590292e7ad0782
ard-hmd <ec2-user@ip-172-31-36-140.eu-west-3.compute.internal>
2023-09-12 15:33:07 +0000

    modify vars name

--- outputs.tf
+++ outputs.tf
@@ -1,62 +1,45 @@
-# # Outputs related to networking and infrastructure components
+output "vpc_id" {
+  description = "The ID of the VPC."
+  value       = aws_vpc.custom_vpc.id
+}
 
-# # VPC ID
-# output "vpc_id" {
-#     value = vpc.vpc_id
-# }
+output "public_subnets_ids" {
+  description = "The IDs of the public subnets."
+  value       = aws_subnet.public_subnet.*.id
+}
 
-# # IDs of public subnets
-# output "public_subnet_ids" {
-#     value = var.public_subnets_cidr
-# }
+output "private_subnets_ids" {
+  description = "The IDs of the private subnets."
+  value       = aws_subnet.private_subnet.*.id
+}
 
-# # IDs of private subnets
-# output "private_subnet_ids" {
-#     value = var.public_subnets_cidr
-# }
+output "rds_subnet_group_name" {
+  description = "The name of the RDS subnet group."
+  value       = aws_db_subnet_group.rds_subnet_group.name
+}
 
-# # IDs of NAT Gateways
-# output "nat_gateway_ids" {
-#   value = gateways.nat_gateway_ids
-# }
+output "public_route_table_ids" {
+  description = "The ID of the public route table."
+  value       = aws_route_table.public.id
+}
 
-# # IDs of AWS Internet Gateways
-# output "aws_internet_gateway_ids" {
-#   value = gateways.aws_internet_gateway_ids
-# }
+output "private_route_table_ids" {
+  description = "The IDs of the private route tables."
+  value       = aws_route_table.private.*.id
+}
 
-# # IDs of private route tables
-# output "private_route_table_ids" {
-#   value = route_tables.private_route_table_ids
-# }
+output "internet_gateway_id" {
+  description = "The ID of the internet gateway."
+  value       = aws_internet_gateway.ig.id
+}
 
-# # ID of the public route table
-# output "public_route_table_id" {
-#   value = route_tables.public_route_table_id
-# }
+output "nat_gateways_ids" {
+  description = "The IDs of the NAT gateways."
+  value       = aws_nat_gateway.nat.*.id
+}
 
-# # Outputs related to Security Groups
+output "elastic_ips" {
+  description = "The Elastic IPs associated with the NAT gateways."
+  value       = aws_eip.nat_eip.*.public_ip
+}
 
-# # ID of the RDS security group
-# output "sg_rds_id" {
-#   description = "The ID of the RDS security group"
-#   value       = sg_rds.sg_rds_id
-# }
-
-# # Name of the RDS subnet group
-# output "main_rds_subnet_group_name" {
-#   description = "The name of the RDS subnet group from the module."
-#   value       = subnets.rds_subnet_group_name
-# }
-
-# # Subnet IDs in the RDS subnet group
-# output "main_rds_subnet_group_subnet_ids" {
-#   description = "The list of subnet IDs in the RDS subnet group from the module."
-#   value       = subnets.rds_subnet_group_subnet_ids
-# }
-
-# # Tags of the RDS subnet group
-# output "main_rds_subnet_group_tags" {
-#   description = "The tags for the RDS subnet group from the module."
-#   value       = subnets.rds_subnet_group_tags
-# }
