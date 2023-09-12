3deaeced77b898f57fb36a61107cf4d005e325c4
ard-hmd <ec2-user@ip-172-31-36-140.eu-west-3.compute.internal>
2023-09-12 13:57:24 +0000

    modify vars name

--- main.tf
+++ main.tf
@@ -19,7 +19,7 @@ resource "aws_vpc" "vpc" {
 # Create AWS public subnets
 resource "aws_subnet" "public_subnet" {
   count                   = length(var.public_subnets_cidr)  # Create multiple subnets based on the count of provided CIDR blocks
-  vpc_id                  = var.vpc_id  # Associate the subnets with the specified VPC
+  vpc_id                  = aws_vpc.vpc_id  # Associate the subnets with the specified VPC
   cidr_block              = element(var.public_subnets_cidr, count.index)  # Use the CIDR block from the list based on the count index
   availability_zone       = element(var.availability_zones, count.index)  # Use the availability zone from the list based on the count index
   map_public_ip_on_launch = true  # Enable automatic public IP assignment for instances launched in this subnet
